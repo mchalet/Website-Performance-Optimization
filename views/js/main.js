3b9daeaf6eef121aca64f5614e2b7e67a575bd27
Kevin <mchaletkevin@gmail.com>
2015-10-06 09:20:48 -0400

    Performance issue fix for scrolling pizzas.

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scrollcalc = (document.body.scrollTop / 1250); // Removed from loop for performance issues.
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scrollcalc) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
