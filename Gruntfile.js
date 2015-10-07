dfaa508924f0becb40c343619760480d1aba74f2
Kevin <mchaletkevin@gmail.com>
2015-10-07 11:02:32 -0400

    Addition of grunt files.

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+// Load "uglify" plugin
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+// Project configuration
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	uglify: {
+	}
+});
+
+
+// Run "uglify" plugin
+
+}
